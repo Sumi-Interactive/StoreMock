46e06672d22e8f92c7b26e59c58b48261939653a
Jun Lin <linjunpop@gmail.com>
2014-03-17 17:43:20 +0800

    Hide ‘Start’ button when displaying ‘Setting’.

--- javascripts/setting.js
+++ javascripts/setting.js
@@ -1,7 +1,9 @@
 $('*[data-behavior=show_setting]').on('click', function(event) {
   $('#setting').slideDown();
+  $('*[data-behavior=show_setting]').hide();
 });
 
 $('*[data-behavior=hide_setting]').on('click', function(event) {
   $('#setting').slideUp();
+  $('*[data-behavior=show_setting]').fadeIn();
 });
