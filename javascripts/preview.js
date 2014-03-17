4cd939256303dcdd732be901090fd4e07f521ca6
Vito <zhangwei.noair@gmail.com>
2014-03-17 18:50:42 +0800

    Optimize code

--- javascripts/preview.js
+++ javascripts/preview.js
@@ -1,14 +1,12 @@
 $(document).ready(function($) {
   $('#pre_iPhone').click(function(event) {
-    $('#pre_frame').removeClass().addClass('iPhone');
-    $('#preview section').removeClass().addClass('iPhone');
-    $("#pre_body").removeClass().addClass('iPhone');
+    $('#preview section.iPhone').show();
+    $('#preview section.iPad').hide();
   });
 
   $('#pre_iPad').click(function(event) {
-    $('#pre_frame').removeClass().addClass('iPad');
-    $('#preview section').removeClass().addClass('iPad');
-    $("#pre_body").removeClass().addClass('iPad');
+    $('#preview section.iPhone').hide();
+    $('#preview section.iPad').show();
   });
 
   $('#pre_mac').click(function(event) {
