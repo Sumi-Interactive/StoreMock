b7b41f6cd974c9d01a8d0a3970d7000fa58661db
Jun Lin <linjunpop@gmail.com>
2014-03-17 14:57:28 +0800

    Support Drag & Drop.

--- file.js
+++ file.js
@@ -1,7 +1,11 @@
 function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
+  evt.stopPropagation();
+  evt.preventDefault();
 
-  // Loop through the FileList and render image files as thumbnails.
+  var files = evt.dataTransfer.files; // FileList object.
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
   for (var i = 0, f; f = files[i]; i++) {
 
     // Only process image files.
@@ -27,4 +31,13 @@ function handleFileSelect(evt) {
   }
 }
 
-document.getElementById('file-files').addEventListener('change', handleFileSelect, false);
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+// Setup the dnd listeners.
+var dropZone = document.getElementById('file-drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
