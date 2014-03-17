d6fe139b1b2f686df15b2258cac3b306f915e830
Vito <zhangwei.noair@gmail.com>
2014-03-17 19:35:27 +0800

    Add Coming soon

--- javascripts/preview.js
+++ javascripts/preview.js
@@ -2,19 +2,29 @@ $(document).ready(function($) {
   $('#pre_iPhone').click(function(event) {
     $('#preview section.iPhone').show();
     $('#preview section.iPad').hide();
+    $('#preview section.mac').hide();
+    $('#preview section.web').hide();
   });
 
   $('#pre_iPad').click(function(event) {
     $('#preview section.iPhone').hide();
     $('#preview section.iPad').show();
+    $('#preview section.mac').hide();
+    $('#preview section.web').hide();
   });
 
   $('#pre_mac').click(function(event) {
-    
+    $('#preview section.iPhone').hide();
+    $('#preview section.iPad').hide();
+    $('#preview section.mac').show();
+    $('#preview section.web').hide();
   });
 
   $('#pre_web').click(function(event) {
-    
+    $('#preview section.iPhone').hide();
+    $('#preview section.iPad').hide();
+    $('#preview section.mac').hide();
+    $('#preview section.web').show();
   });
 
 });
