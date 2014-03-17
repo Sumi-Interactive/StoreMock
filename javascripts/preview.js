3e47e03fec733b5259462aff9df6b2649d16dc98
Vito <zhangwei.noair@gmail.com>
2014-03-17 18:22:56 +0800

    Replace frame to div

--- javascripts/preview.js
+++ javascripts/preview.js
@@ -2,13 +2,13 @@ $(document).ready(function($) {
   $('#pre_iPhone').click(function(event) {
     $('#pre_frame').removeClass().addClass('iPhone');
     $('#preview section').removeClass().addClass('iPhone');
-    $('#pre_frame').contents().find("#pre_body").removeClass().addClass('iPhone');
+    $("#pre_body").removeClass().addClass('iPhone');
   });
 
   $('#pre_iPad').click(function(event) {
     $('#pre_frame').removeClass().addClass('iPad');
     $('#preview section').removeClass().addClass('iPad');
-    $('#pre_frame').contents().find("#pre_body").removeClass().addClass('iPad');
+    $("#pre_body").removeClass().addClass('iPad');
   });
 
   $('#pre_mac').click(function(event) {
